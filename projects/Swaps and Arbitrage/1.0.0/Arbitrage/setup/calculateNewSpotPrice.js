598a512277d80431052c4767c07617b6d5ba8b2c
Daniel Nolan <DanNolan99@gmail.com>
2021-03-08 11:30:19 -0500

    fixes new spot price initial solution

--- projects/Swaps and Arbitrage/1.0.0/Arbitrage/setup/calculateNewSpotPrice.js
+++ projects/Swaps and Arbitrage/1.0.0/Arbitrage/setup/calculateNewSpotPrice.js
@@ -1,3 +1,10 @@
+const calculateOut = require('./calculateOut');
+const poolInfo = require('./poolInfo');
+
+/**
+ * @param {Contract} pool A Contract pointed at a BPool
+ * @param {BigNumber} daiIn Amount of DAI to be swapped in
+ */
 async function calculateNewSpotPrice(pool, daiIn, wethOut) {
     
 }
